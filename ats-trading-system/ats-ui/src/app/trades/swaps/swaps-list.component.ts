c21212175f5fd02ef7b389b7671b3034f4ab7d77
Mutwila <mutwila.m@alintatechsolutions.co.za>
2025-10-26 01:39:38 +0200

    changes to UI code errors

--- ats-trading-system/ats-ui/src/app/trades/swaps/swaps-list.component.ts
+++ ats-trading-system/ats-ui/src/app/trades/swaps/swaps-list.component.ts
@@ -1,5 +1,9 @@
 // trades/swaps/swaps-list.component.ts
-@Component({ standalone:true, template:`<div class="p-6"><h1 class="text-xl font-semibold">Swaps</h1>
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({ standalone:true,  imports: [CommonModule], template:`<div class="p-6"><h1 class="text-xl font-semibold">Swaps</h1>
 <button (click)="load()">Load</button><pre>{{data | json}}</pre></div>`})
 export class SwapsListComponent {
   data:any; async load(){ this.data = await fetch('/swaps').then(r=>r.json()); }
